9e746f6e4a1c4bcae9303062ff780806abc3c401
sibgatulislam@gmail.com <sibgatulislam@gmail.com>
2023-08-28 11:00:19 +0600

    clear

--- main.py
+++ main.py
@@ -1,72 +1,38 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 import numpy as np
-from collections import defaultdict
-import tensorflow as tf;
+import matplotlib.pyplot as plt
+import seaborn as sns
+import tensorflow as tf
+import keras
+from keras import layers
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from scipy import stats
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
+def make_model(input_shape, x_train, y_train, x_test, y_test):
+    model = tf.keras.models.Sequential([
+        layers.Dense(64, activation='relu', input_shape=[input_shape]),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    model.compile(loss='mae', optimizer='adam', metrics=['accuracy'])
+    history = model.fit(x_train, y_train, epochs=100, validation_split=0.2, verbose=0, callbacks=[early_stop])
+    hist = pd.DataFrame(history.history)
+    print(hist.tail())
 
 train_data = pd.read_csv('./home_data/train.csv')
 test_data = pd.read_csv('./home_data/test.csv')
 
-df_train = train_data.copy()
-df_test = test_data.copy()
-salesPrice = df_train['SalePrice']
-def plotData(data, var):
-    data = pd.concat([data['SalePrice'], data[var]], axis=1)
-    data.plot.scatter(x=var, y='SalePrice', ylim=(0, 800000));
-    plt.show()
+train_data = train_data.drop(['Id'], axis=1)
+test_data = test_data.drop(['Id'], axis=1)
+numeric_cols = train_data.select_dtypes(include=[np.number]).columns
+train_data = train_data[numeric_cols]
+train_data = train_data.astype('float32')
+train_data = train_data.fillna(train_data.mean())
 
-def displot(data, var):
-    sns.displot(data[var], kde=True, color='b');
-    plt.figure()
-    res = stats.probplot(data[var], plot=plt)
-    plt.show()
-
-# plotData(df_train, 'GrLivArea')
-# plotData(df_train, 'TotalBsmtSF')
-# plotData(df_train, 'OverallQual')
-
-corrmat = df_train.corr(numeric_only=True);
-f, ax = plt.subplots(figsize=(12, 9))
-sns.heatmap(corrmat, square=True);
-#plt.show()
-
-cols = corrmat.nlargest(10, 'SalePrice')['SalePrice'].index
-cm = np.corrcoef(df_train[cols].values.T)
-#print(cm)
-sns.set(font_scale=1.25);
-hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'size': 10}, yticklabels=cols.values, xticklabels=cols.values)
-#plt.show()
-rem_cols = ['GarageArea', '1stFlrSf']
-
-cols = [col for col in cols if col not in rem_cols]
-sns.pairplot(df_train[cols], height=2.5)
-#plt.show()
-
-total = df_train.isnull().sum().sort_values(ascending=False)
-percent = (df_train.isnull().sum() / df_train.isnull().count()).sort_values(ascending=False)
-
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent']);
-#print(missing_data.head(20))
-
-df_train = df_train.drop((missing_data[missing_data['Total'] > 1]).index, 1)
-df_train['Electrical'] = df_train['Electrical'].fillna('Mix')
-#df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
-
-salesPrice_scaled = StandardScaler().fit_transform(df_train['SalePrice'][:, np.newaxis]);
-low_range = salesPrice_scaled[salesPrice_scaled[:, 0].argsort()][:10]
-high_range = salesPrice_scaled[salesPrice_scaled[:, 0].argsort()][-10:]
-
-df_train['SalePrice'] = np.log(df_train['SalePrice'])
-df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
-df_train['TotalBsmtSF'] = np.log(df_train['TotalBsmtSF'])
-
-displot(df_train, 'SalePrice')
-displot(df_train, 'GrLivArea')
-displot(df_train, 'TotalBsmtSF')
-plotData(df_train, 'GrLivArea')
-plotData(df_train, 'TotalBsmtSF')
-
-print(salesPrice_scaled)
+x_train, x_test, y_train, y_test = train_test_split(train_data, train_data['SalePrice'], train_size=.8, test_size=0.2, random_state=0)
+make_model(len(numeric_cols), x_train, y_train, x_test, y_test)
+print(train_data.head(), x_train.head(), y_train.head())
